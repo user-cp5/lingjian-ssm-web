67b471f67bf07fa9c581732ad9fa3445f92839fd
lingjian <lingjian@163.com>
2019-02-28 21:28:58 +0800

    测试git 提交

---
+++ src/com/itheima/test/Test.java
@@ -0,0 +1,36 @@
+package com.itheima.test;
+
+public class Test {
+
+    public static void main(String [] args){
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssss");
+    }
+}
